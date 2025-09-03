ab335600e221283566e3f372c76c3c11df2b944b
webDevWay <k.vasiliy.r@gmail.com>
2025-09-03 18:42:27 +0800

    2st step

--- Makefile
+++ Makefile
@@ -2,7 +2,13 @@ install:
 	composer install
 
 brain-games:
-	./bin/brain-games
+	php ./bin/brain-games
+
+brain-even:
+	php ./bin/brain-even
 
 validate:
 	composer validate
+
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
--- src/Cli.php
+++ src/Cli.php
@@ -1,13 +1,15 @@
 <?php
 namespace BrainGames\Cli;
-
-use function cli\line;
-use function cli\prompt;
-
+//use function cli\line;
+//use function cli\prompt;
+/**
+ * Greeting user
+*/
 function getName()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    //line('Welcome to the Brain Game!');
+    //$name = prompt('May I have your name?');
+    //line("Hello, %s!", $name);
+    echo "PROGIM!";
 }
 
