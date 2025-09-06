8a96f48f5515408e9a588b57c8cb5201d575fd2a
webDevWay <k.vasiliy.r@gmail.com>
2025-09-07 07:59:13 +0800

    4st step

--- src/Cli.php
+++ src/Cli.php
@@ -1,15 +1,15 @@
 <?php
+
 namespace BrainGames\Cli;
-//use function cli\line;
-//use function cli\prompt;
-/**
- * Greeting user
-*/
+
+// Use Composer's autoloader:
+use function cli\line;
+use function cli\prompt;
+
+/* Greeting user  */
 function getName()
 {
-    //line('Welcome to the Brain Game!');
-    //$name = prompt('May I have your name?');
-    //line("Hello, %s!", $name);
-    echo "PROGIM!";
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
-
